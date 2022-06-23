20c5bcfd0ec42f5e69f637130b24fab85117274a
Ta-Muna <tamaripilia@gmail.com>
2022-06-23 21:09:39 +0400

    Added the proper iconElement from API

--- Source/javaScript.js
+++ Source/javaScript.js
@@ -40,6 +40,12 @@ function showNewTemperatureAndDescription(response) {
 
   document.querySelector(`#description`).innerHTML =
     response.data.weather[0].main;
+  let iconElement = document.querySelector(`#icon`);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].main);
 }
 
 // Search Engine
